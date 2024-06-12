2fe3165e65a5b74209b460c7827f7240665b9ddc
Fitroh Ayu Wulandari <115079204+123fitroh@users.noreply.github.com>
2024-06-12 08:49:00 +0000

    Membuat db Reset

---
+++ db/migrate/20240612074558_create_articles.rb
@@ -0,0 +1,11 @@
+class CreateArticles < ActiveRecord::Migration[6.0]
+  def change
+    create_table :articles do |t|
+      t.belong_to :user
+      t.string :tittle
+      t.text :description
+
+      t.timestamps # created_at, updated_at berupa datetime
+    end
+  end
+end
