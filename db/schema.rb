8650e81707da7b76ceae6ea65e1e08627916345c
Fitroh Ayu Wulandari <115079204+123fitroh@users.noreply.github.com>
2024-06-13 08:06:51 +0000

    Update latest SC

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_06_12_083005) do
+ActiveRecord::Schema.define(version: 2024_06_13_002208) do
 
   create_table "articles", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -24,7 +24,8 @@ ActiveRecord::Schema.define(version: 2024_06_12_083005) do
   create_table "comments", force: :cascade do |t|
     t.integer "article_id", null: false
     t.string "comment"
-    t.datetime "created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
     t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
